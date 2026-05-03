9bd9f4f1c5659378b057b59945d273f98c914e81
danurifai <rifaidanu12@gmail.com>
2026-05-03 20:10:41 +0700

    source code

--- inventory.py
+++ inventory.py
@@ -1,7 +1,25 @@
-class Item:
-    def __init__(self, nama, stok):
-        self.nama = nama
-        self.stok = stok
+owner = {'O1': {'nama': 'bani',
+                'usia': 19,
+                'alamt': 'wonosobo'}}
 
-class Inventory:
-    print ("inventory")
+while True:
+
+    print(f'''
+pilihan:
+1. Update
+2. Cetak data
+''')
+    
+    input_pilihan = input("masukan pilihan: ")
+    
+    if input_pilihan == '1':
+        data_target = input('data target: ')
+        input_update = input('masukan update: ')
+
+        def update_data(data_target, data_baru):
+            owner[data_target]['nama'] = input_update
+
+        update_data(data_target, input_update)
+
+    elif input_pilihan == "2":
+        print(owner['O1']['nama'])
